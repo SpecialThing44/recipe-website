79803c757e9eb9820ad00a09826a5bb017d87cf7
Spencer Perkins <95900100+SpecialThing44@users.noreply.github.com>
2026-04-14 14:20:25 -0400

    Seperate scoring parts into separate file

--- cooking-backend/src/main/scala/persistence/filters/FiltersConverter.scala
+++ cooking-backend/src/main/scala/persistence/filters/FiltersConverter.scala
@@ -3,6 +3,7 @@ package persistence.filters
 import domain.filters.Filters
 import io.circe.syntax.EncoderOps
 import persistence.users.UserConverter.lowerPrefix
+import persistence.filters.CypherScoringParts._
 
 object FiltersConverter {
   private def similarityActive(filters: Filters): Boolean =
@@ -29,9 +30,6 @@ object FiltersConverter {
     else if (includeIngredient) s"$nodeVar, target, ingredientScore"
     else if (includeCoSave) s"$nodeVar, target, coSaveScore"
     else s"$nodeVar, target"
-
-  private def appendMinWhere(scoreName: String, min: Option[Double]): String =
-    min.map(minValue => s"\nWHERE $scoreName >= $minValue ").getOrElse("")
 
   private def buildFinalScoreWith(
       nodeVar: String,
@@ -184,18 +182,7 @@ object FiltersConverter {
 
       val ingredientPart =
         if (!ingredientActiveRecipe) ""
-        else
-          s"""
-           |MATCH (target)-[tr:HAS_INGREDIENT]->(ti:Ingredient)
-           |WITH $nodeVar, target, collect({ingredientId: ti.id, weight: coalesce(tr.normalizedWeight, 0.0)}) AS targetVector
-           |MATCH ($nodeVar)-[ci:HAS_INGREDIENT]->(ciIngr:Ingredient)
-           |WITH $nodeVar, target, targetVector, collect({ingredientId: ciIngr.id, weight: coalesce(ci.normalizedWeight, 0.0)}) AS candidateVector
-           |WITH $nodeVar, target, targetVector, candidateVector,
-           |     reduce(dot = 0.0, x IN targetVector | dot + coalesce([y IN candidateVector WHERE y.ingredientId = x.ingredientId][0].weight, 0.0) * x.weight) AS dotProd,
-           |     sqrt(reduce(s = 0.0, x IN targetVector | s + x.weight * x.weight)) AS normTarget,
-           |     sqrt(reduce(s = 0.0, x IN candidateVector | s + x.weight * x.weight)) AS normCandidate
-           |WITH $nodeVar, target, CASE WHEN normTarget = 0 OR normCandidate = 0 THEN 0.0 ELSE dotProd / (normTarget * normCandidate) END AS ingredientScore
-           |""".stripMargin + appendMinWhere("ingredientScore", ingredientMin)
+        else recipeRecipeIngredientPart(nodeVar, ingredientMin)
 
       val coSaveCarry = carryFields(
         nodeVar,
@@ -204,20 +191,7 @@ object FiltersConverter {
       )
       val coSavePart =
         if (!coSaveActiveRecipe) ""
-        else
-          s"""
-           |WITH $coSaveCarry
-           |OPTIONAL MATCH (target)-[:SAVED_BY]->(ur:User)
-           |WITH $coSaveCarry, collect(DISTINCT ur.id) AS usersT
-           |OPTIONAL MATCH ($nodeVar)-[:SAVED_BY]->(uc:User)
-           |WITH $coSaveCarry, usersT, collect(DISTINCT uc.id) AS usersC
-           |WITH $coSaveCarry,
-           |     size([x IN usersC WHERE x IN usersT]) AS inter,
-           |     size(usersT) AS sizeT,
-           |     size(usersC) AS sizeC
-           |WITH $coSaveCarry, inter, sizeT, sizeC,
-           |     CASE WHEN sizeT + sizeC - inter = 0 THEN 0.0 ELSE toFloat(inter) / toFloat(sizeT + sizeC - inter) END AS coSaveScore
-           |""".stripMargin + appendMinWhere("coSaveScore", coSaveMin)
+        else recipeRecipeCoSavePart(nodeVar, coSaveCarry, coSaveMin)
 
       val tagCarryBase = carryFields(
         nodeVar,
@@ -226,17 +200,7 @@ object FiltersConverter {
       )
       val tagPart =
         if (!tagActiveRecipe) ""
-        else
-          s"""
-           |WITH $tagCarryBase
-           |OPTIONAL MATCH (target)-[:HAS_TAG]->(tt:Tag)
-           |WITH $tagCarryBase, collect(DISTINCT tt.name) AS targetTags
-           |OPTIONAL MATCH ($nodeVar)-[:HAS_TAG]->(ct:Tag)
-           |WITH $tagCarryBase, targetTags, collect(DISTINCT ct.name) AS candidateTags
-           |WITH $tagCarryBase, size([x IN candidateTags WHERE x IN targetTags]) AS interTags, size(targetTags) AS sizeTargetTags, size(candidateTags) AS sizeCandidateTags
-           |WITH $tagCarryBase, interTags, sizeTargetTags, sizeCandidateTags,
-           |     CASE WHEN sizeTargetTags + sizeCandidateTags - interTags = 0 THEN 0.0 ELSE toFloat(interTags) / toFloat(sizeTargetTags + sizeCandidateTags - interTags) END AS tagScore
-           |""".stripMargin + appendMinWhere("tagScore", tagMin)
+        else recipeRecipeTagPart(nodeVar, tagCarryBase, tagMin)
 
       val finalWith = buildFinalScoreWith(
         nodeVar,
@@ -258,24 +222,7 @@ object FiltersConverter {
 
       val ingredientPart =
         if (!ingredientActiveRecipe) ""
-        else
-          s"""
-           |OPTIONAL MATCH (target)<-[:SAVED_BY]-(tr:Recipe)
-           |OPTIONAL MATCH (target)<-[:CREATED_BY]-(tcr:Recipe)
-           |WITH $nodeVar, target, (collect(DISTINCT tr) + collect(DISTINCT tcr)) AS recipesT
-           |WHERE NOT $nodeVar IN recipesT
-           |UNWIND recipesT AS tRec
-           |MATCH (tRec)-[tr:HAS_INGREDIENT]->(ti:Ingredient)
-           |WITH $nodeVar, target, ti.id AS ingrIdT, sum(coalesce(tr.normalizedWeight, 0.0)) AS weightT
-           |WITH $nodeVar, target, collect({ingredientId: ingrIdT, weight: weightT}) AS targetVector
-           |MATCH ($nodeVar)-[ci:HAS_INGREDIENT]->(ciIngr:Ingredient)
-           |WITH $nodeVar, target, targetVector, collect({ingredientId: ciIngr.id, weight: coalesce(ci.normalizedWeight, 0.0)}) AS candidateVector
-           |WITH $nodeVar, target, targetVector, candidateVector,
-           |     reduce(dot = 0.0, x IN targetVector | dot + coalesce([y IN candidateVector WHERE y.ingredientId = x.ingredientId][0].weight, 0.0) * x.weight) AS dotProd,
-           |     sqrt(reduce(s = 0.0, x IN targetVector | s + x.weight * x.weight)) AS normTarget,
-           |     sqrt(reduce(s = 0.0, x IN candidateVector | s + x.weight * x.weight)) AS normCandidate
-           |WITH $nodeVar, target, CASE WHEN normTarget = 0 OR normCandidate = 0 THEN 0.0 ELSE dotProd / (normTarget * normCandidate) END AS ingredientScore
-           |""".stripMargin + appendMinWhere("ingredientScore", ingredientMin)
+        else userRecipeIngredientPart(nodeVar, ingredientMin)
 
       val tagCarryBase = carryFields(
         nodeVar,
@@ -285,22 +232,7 @@ object FiltersConverter {
 
       val tagPart =
         if (!tagActiveRecipe) ""
-        else
-          s"""
-           |WITH $tagCarryBase
-           |OPTIONAL MATCH (target)<-[:SAVED_BY]-(tr:Recipe)
-           |OPTIONAL MATCH (target)<-[:CREATED_BY]-(tcr:Recipe)
-           |WITH $tagCarryBase, (collect(DISTINCT tr) + collect(DISTINCT tcr)) AS recipesT
-           |WHERE NOT $nodeVar IN recipesT
-           |UNWIND recipesT AS tRec
-           |MATCH (tRec)-[:HAS_TAG]->(tt:Tag)
-           |WITH $tagCarryBase, collect(DISTINCT tt.name) AS targetTags
-           |OPTIONAL MATCH ($nodeVar)-[:HAS_TAG]->(ct:Tag)
-           |WITH $tagCarryBase, targetTags, collect(DISTINCT ct.name) AS candidateTags
-           |WITH $tagCarryBase, size([x IN candidateTags WHERE x IN targetTags]) AS interTags, size(targetTags) AS sizeTargetTags, size(candidateTags) AS sizeCandidateTags
-           |WITH $tagCarryBase, interTags, sizeTargetTags, sizeCandidateTags,
-           |     CASE WHEN sizeTargetTags + sizeCandidateTags - interTags = 0 THEN 0.0 ELSE toFloat(interTags) / toFloat(sizeTargetTags + sizeCandidateTags - interTags) END AS tagScore
-           |""".stripMargin + appendMinWhere("tagScore", tagMin)
+        else userRecipeTagPart(nodeVar, tagCarryBase, tagMin)
 
       val anyMinApplied = hasAppliedMinThreshold(
         ingredientActiveRecipe -> ingredientMin,
@@ -331,32 +263,7 @@ object FiltersConverter {
 
       val ingredientPart =
         if (!ingredientActiveUser) ""
-        else
-          s"""
-             |OPTIONAL MATCH (ts:Recipe)-[:SAVED_BY]->(target)
-             |OPTIONAL MATCH (tc:Recipe)-[:CREATED_BY]->(target)
-             |OPTIONAL MATCH (cs:Recipe)-[:SAVED_BY]->($nodeVar)
-             |OPTIONAL MATCH (cc:Recipe)-[:CREATED_BY]->($nodeVar)
-             |WITH $nodeVar, target,
-             |     collect(DISTINCT ts) + collect(DISTINCT tc) AS tAll,
-             |     collect(DISTINCT cs) + collect(DISTINCT cc) AS cAll
-             |WITH $nodeVar, target, tAll, cAll,
-             |     CASE WHEN size(tAll) = 0 THEN [null] ELSE tAll END AS tAllNZ,
-             |     CASE WHEN size(cAll) = 0 THEN [null] ELSE cAll END AS cAllNZ
-             |UNWIND tAllNZ AS tRec
-             |OPTIONAL MATCH (tRec)-[tr:HAS_INGREDIENT]->(ti:Ingredient)
-             |WITH $nodeVar, target, ti.id AS ingrIdT, sum(coalesce(tr.normalizedWeight, 0.0)) AS weightT, cAllNZ
-             |WITH $nodeVar, target, collect({ingredientId: ingrIdT, weight: weightT}) AS targetVector, cAllNZ
-             |UNWIND cAllNZ AS cRec
-             |OPTIONAL MATCH (cRec)-[ci:HAS_INGREDIENT]->(ciIngr:Ingredient)
-             |WITH $nodeVar, target, targetVector, ciIngr.id AS ingrIdC, sum(coalesce(ci.normalizedWeight, 0.0)) AS weightC
-             |WITH $nodeVar, target, targetVector, collect({ingredientId: ingrIdC, weight: weightC}) AS candidateVector
-             |WITH $nodeVar, target, targetVector, candidateVector,
-             |     reduce(dot = 0.0, x IN targetVector | dot + coalesce([y IN candidateVector WHERE y.ingredientId = x.ingredientId][0].weight, 0.0) * x.weight) AS dotProd,
-             |     sqrt(reduce(s = 0.0, x IN targetVector | s + x.weight * x.weight)) AS normTarget,
-             |     sqrt(reduce(s = 0.0, x IN candidateVector | s + x.weight * x.weight)) AS normCandidate
-             |WITH $nodeVar, target, CASE WHEN normTarget = 0 OR normCandidate = 0 THEN 0.0 ELSE dotProd / (normTarget * normCandidate) END AS ingredientScore
-             |""".stripMargin + appendMinWhere("ingredientScore", ingredientMin)
+        else userUserIngredientPart(nodeVar, ingredientMin)
 
       val coSaveCarry = carryFields(
         nodeVar,
@@ -365,22 +272,7 @@ object FiltersConverter {
       )
       val coSavePart =
         if (!coSaveActiveUser) ""
-        else
-          s"""
-           |WITH $coSaveCarry
-           |OPTIONAL MATCH (target)<-[:SAVED_BY]-(tr:Recipe)
-           |OPTIONAL MATCH (target)<-[:CREATED_BY]-(tcr:Recipe)
-           |WITH $coSaveCarry, (collect(DISTINCT tr.id) + collect(DISTINCT tcr.id)) AS recipesT
-           |OPTIONAL MATCH ($nodeVar)<-[:SAVED_BY]-(cr:Recipe)
-           |OPTIONAL MATCH ($nodeVar)<-[:CREATED_BY]-(ccr:Recipe)
-           |WITH $coSaveCarry, recipesT, (collect(DISTINCT cr.id) + collect(DISTINCT ccr.id)) AS recipesC
-           |WITH $coSaveCarry,
-           |     size([x IN recipesC WHERE x IN recipesT]) AS inter,
-           |     size(recipesT) AS sizeT,
-           |     size(recipesC) AS sizeC
-           |WITH $coSaveCarry, inter, sizeT, sizeC,
-           |     CASE WHEN sizeT + sizeC - inter = 0 THEN 0.0 ELSE toFloat(inter) / toFloat(sizeT + sizeC - inter) END AS coSaveScore
-           |""".stripMargin + appendMinWhere("coSaveScore", coSaveMin)
+        else userUserCoSavePart(nodeVar, coSaveCarry, coSaveMin)
 
       val tagCarryBase = carryFields(
         nodeVar,
@@ -389,32 +281,7 @@ object FiltersConverter {
       )
       val tagPart =
         if (!tagActiveUser) ""
-        else
-          s"""
-             |WITH $tagCarryBase
-             |OPTIONAL MATCH (tss:Recipe)-[:SAVED_BY]->(target)
-             |OPTIONAL MATCH (tcc:Recipe)-[:CREATED_BY]->(target)
-             |OPTIONAL MATCH (css:Recipe)-[:SAVED_BY]->($nodeVar)
-             |OPTIONAL MATCH (ccc:Recipe)-[:CREATED_BY]->($nodeVar)
-             |WITH $tagCarryBase,
-             |     collect(DISTINCT tss) + collect(DISTINCT tcc) AS tAllTags,
-             |     collect(DISTINCT css) + collect(DISTINCT ccc) AS cAllTags
-             |WITH $tagCarryBase, tAllTags, cAllTags,
-             |     CASE WHEN size(tAllTags) = 0 THEN [null] ELSE tAllTags END AS tAllTagsNZ,
-             |     CASE WHEN size(cAllTags) = 0 THEN [null] ELSE cAllTags END AS cAllTagsNZ
-             |UNWIND tAllTagsNZ AS tTagRec
-             |OPTIONAL MATCH (tTagRec)-[:HAS_TAG]->(tt:Tag)
-             |WITH $tagCarryBase, cAllTagsNZ, collect(DISTINCT tt.name) AS targetTags
-             |UNWIND cAllTagsNZ AS cTagRec
-             |OPTIONAL MATCH (cTagRec)-[:HAS_TAG]->(ct:Tag)
-             |WITH $tagCarryBase, targetTags, collect(DISTINCT ct.name) AS candidateTags
-             |WITH $tagCarryBase,
-             |     size([x IN candidateTags WHERE x IN targetTags]) AS interTags,
-             |     size(targetTags) AS sizeTargetTags,
-             |     size(candidateTags) AS sizeCandidateTags
-             |WITH $tagCarryBase, interTags, sizeTargetTags, sizeCandidateTags,
-             |     CASE WHEN sizeTargetTags + sizeCandidateTags - interTags = 0 THEN 0.0 ELSE toFloat(interTags) / toFloat(sizeTargetTags + sizeCandidateTags - interTags) END AS tagScore
-             |""".stripMargin + appendMinWhere("tagScore", tagMin)
+        else userUserTagPart(nodeVar, tagCarryBase, tagMin)
 
       val anyMinApplied = hasAppliedMinThreshold(
         ingredientActiveUser -> ingredientMin,
